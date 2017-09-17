9cb70c8be84c075ac78ddfddc7ec1848fa42349d
Brennan Gamwell <brennan@segment.com>
2017-09-17 16:49:49 -0700

    Add core int library/working on tests

--- src/main/java/com/segment/analytics/android/integrations/optimizelyx/OptimizelyXIntegration.java
+++ src/main/java/com/segment/analytics/android/integrations/optimizelyx/OptimizelyXIntegration.java
@@ -1,63 +1,104 @@
 package com.segment.analytics.android.integrations.optimizelyx;
 
-import android.app.Application;
 import android.content.Context;
-import android.os.Bundle;
+
+import com.optimizely.ab.config.Experiment;
+import com.optimizely.ab.event.LogEvent;
 import com.segment.analytics.Analytics;
+import com.segment.analytics.Options;
+import com.segment.analytics.Traits;
 import com.segment.analytics.Properties;
 import com.segment.analytics.ValueMap;
-import com.segment.analytics.integrations.IdentifyPayload;
 import com.segment.analytics.integrations.Integration;
 import com.segment.analytics.integrations.Logger;
-import com.segment.analytics.integrations.TrackPayload;
-import java.util.List;
+
+import static com.segment.analytics.internal.Utils.isNullOrEmpty;
+
+import com.optimizely.ab.Optimizely;
+import com.optimizely.ab.notification.NotificationListener;
+import com.optimizely.ab.config.Variation;
+
+import java.util.Map;
 
 /**
- * Kahuna helps mobile marketers send push notifications and in-app messages.
+ * Optimizely X helps marketers and growth hackers make smart decisions through A/B testing.
  *
- * @see <a href="https://www.optimizely.com/">Pptimizely</a>
- * @see <a href="https://segment.com/docs/integrations/optimizely/">Optimizely Integration</a>
- * @see <a href="http://developers.optimizely.com/android/">Optimizely Android SDK</a>
+ * @see <a href="https://www.optimizely.com/">Optimizely</a>
+ * @see <a href="https://segment.com/docs/destinations/optimizelyx/">Optimizely Integration</a>
+ * @see <a href="https://developers.optimizely.com/x/solutions/sdks/introduction/index.html?language=android&platform=mobile">Optimizely Android SDK</a>
  */
-public class OptimizelyXIntegration extends Integration<Void> {
-  private static final String OPTIMIZELY_KEY = "Optimizely";
+public class OptimizelyXIntegration extends Integration<NotificationListener> {
 
-  private final Context context;
+  private static final String OPTIMIZELYX_KEY = "Optimizely X";
   private final Logger logger;
-  private final Analytics analytics;
+  final NotificationListener listener;
 
-  // Optimizely must be initialized immediately on launch. So we require the token when creating the
-  // factory, and initialize it in this method.
-//  public static Factory createFactory(final Application application, String token) {
-////    Optimizely.startOptimizelyWithAPIToken(token, application);
-////    return new Factory() {
-////      @Override public Integration<?> create(ValueMap settings, Analytics analytics) {
-////        boolean listen = settings.getBoolean("listen", false);
-////        Logger logger = analytics.logger(OPTIMIZELY_KEY);
-////
-////        OptimizelyIntegration integration =
-////            new OptimizelyIntegration(analytics, application, logger);
-////        if (listen) {
-////          Optimizely.addOptimizelyEventListener(integration);
-////        }
-////        return integration;
-////      }
-//
-////      @Override public String key() {
-////      return OPTIMIZELY_KEY;
-//////      }
-//    };
-//  }
+  public static final Factory FACTORY =
+          new Factory() {
+            @Override
+            public Integration<?> create(ValueMap settings, Analytics analytics) {
+              Logger logger = analytics.logger(OPTIMIZELYX_KEY);
+              Context context = analytics.getApplication();
+              return new OptimizelyXIntegration(analytics, settings, logger);
+            }
 
-  public OptimizelyXIntegration(Analytics analytics, Context context, Logger logger) {
-    this.analytics = analytics;
-    this.context = context;
+            @Override
+            public String key() { return OPTIMIZELYX_KEY; }
+          };
+
+  public OptimizelyXIntegration(Analytics analytics, ValueMap settings, Logger logger) {
     this.logger = logger;
+    final Analytics finalAnalytics = analytics; // analytics must be set as a final variable to be accessed by "inner class" - why?
+    final Options options = new Options().setIntegration("Optimizely", false);
+
+    listener = new NotificationListener() {
+      @Override
+      public void onEventTracked(String eventKey,
+                                 String userId,
+                                 Map<String, String> attributes,
+                                 Long eventValue,
+                                 LogEvent logEvent) {
+
+        if (!isNullOrEmpty(attributes)) {
+          Traits traits = new Traits();
+          for (Map.Entry<String, String> entry : attributes.entrySet()) {
+            String trait = entry.getKey();
+            String value = entry.getValue();
+            traits.putValue(trait, value);
+          }
+          finalAnalytics.identify(userId, traits, options);
+        }
+        finalAnalytics.track(eventKey);
+      }
+
+      @Override
+      public void onExperimentActivated(Experiment experiment,
+                                        String userId,
+                                        Map<String, String> attributes,
+                                        Variation variation) {
+
+        if (!isNullOrEmpty(attributes)) {
+          Traits traits = new Traits();
+          for (Map.Entry<String, String> entry : attributes.entrySet()) {
+            String trait = entry.getKey();
+            String value = entry.getValue();
+            traits.putValue(trait, value);
+          }
+          finalAnalytics.identify(userId, traits, options);
+        }
+
+        Properties properties = new Properties()
+                .putValue("experimentId", experiment.getId())
+                .putValue("experimentName", experiment.getKey())
+                .putValue("variationId", variation.getId())
+                .putValue("variationName", variation.getKey());
+        finalAnalytics.track("Experiment Viewed", properties, options);
+      }
+    };
   }
 
-  @Override public void identify(IdentifyPayload identify) {
-  }
-
-  @Override public void track(TrackPayload track) {
+  @Override
+  public NotificationListener getUnderlyingInstance() {
+    return listener;
   }
 }
--- src/test/java/com/segment/analytics/android/integrations/optimizelyx/OptimizelyXTest.java
+++ src/test/java/com/segment/analytics/android/integrations/optimizelyx/OptimizelyXTest.java
@@ -1,52 +1,82 @@
 package com.segment.analytics.android.integrations.optimizelyx;
 
 import android.app.Application;
+
+import com.optimizely.ab.Optimizely;
+import com.optimizely.ab.config.ProjectConfigUtils;
+import com.optimizely.ab.config.TrafficAllocation;
+import com.optimizely.ab.notification.NotificationListener;
+import com.optimizely.ab.config.Variation;
+import com.optimizely.ab.config.Experiment;
+
 import com.segment.analytics.Analytics;
 import com.segment.analytics.Properties;
 import com.segment.analytics.core.tests.BuildConfig;
 import com.segment.analytics.integrations.Logger;
 import com.segment.analytics.test.IdentifyPayloadBuilder;
 import com.segment.analytics.test.TrackPayloadBuilder;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static android.R.attr.key;
+import static com.segment.analytics.Analytics.LogLevel.VERBOSE;
+import static com.segment.analytics.Utils.createTraits;
+
 import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.rule.PowerMockRule;
-import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.annotation.Config;
 
-import static com.segment.analytics.Analytics.LogLevel.VERBOSE;
-import static com.segment.analytics.Utils.createTraits;
-import static org.mockito.Mockito.verify;
-import static org.mockito.MockitoAnnotations.initMocks;
-import static org.powermock.api.mockito.PowerMockito.verifyStatic;
-
-@RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 18, manifest = Config.NONE)
-@PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*" })
-//@PrepareForTest({ Optimizely.class }) //
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+@PowerMockIgnore({ "org.mockito.*", "org.roboelectric.*", "android.*" })
+@PrepareForTest({ NotificationListener.class })
 public class OptimizelyXTest {
 
   @Rule public PowerMockRule rule = new PowerMockRule();
-  @Mock Application application;
-  @Mock Analytics analytics;
+  @Mock
+  Analytics analytics;
+
+  OptimizelyXIntegration integration;
 
   @Before public void setUp() {
+    initMocks(this);
+    PowerMockito.mock(NotificationListener.class);
+
+    integration = new OptimizelyXIntegration(analytics, null, Logger.with(VERBOSE));
   }
 
-  @Test public void identify() {
+  @Test public void onEventTracked() {
+    Map<String, String> attributes = new HashMap<>();
+
+    integration.listener.onEventTracked("Experiment Viewed", "123", attributes, null, null);
+
+    verify(analytics).track("Experiment Viewed");
   }
 
-  @Test public void track() {
-  }
+  @Test public void onExperimentActivated() {
 
-  @Test public void trackWithRevenue() {
-  }
+//    Experiment experiment = new Experiment();
+    String userId = "123";
+    Map<String, String> attributes = new HashMap<>();
+//    Variation variation = new Variation();
 
-  @Test public void root() {
+    integration.listener.onExperimentActivated(null, userId, attributes, null);
+
+    verify(analytics).track("Experiment Activated");
   }
 }
