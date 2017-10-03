3b505dfc59aa9081a20b3f4b48ba42a9ab3e11ce
Brennan Gamwell <brennan@segment.com>
2017-10-03 10:01:43 -0700

    Optimizely X v1 integration (#1)
    * Add core int library/working on tests
    
    * Remove superfluous test deps; stub Experiment and Variation objects for testing
    
    * Fix linter errors; add identify method to tests
    
    * Refactor main library; working on updating tests
    
    * Fix tests; fix typos in main lib
    
    * Remove unused import statement/class
    
    * Add trackKnownUsers setting and tests
    
    * Simplify trackKnownUser logic and update tests
    
    * Store track events if Optimizely isn't initialized; add polling to check for valid OptimizelyClient
    
    * Optimize imports; remove superfluous logging; optimize thread
    
    * Update polling thread; fix tests
    
    * Update pollOptimizelyClient method
    
    * Update polling thread, tests, add defaultAttribute mapping
    
    * Add test for polling Optimizely client
    
    * Test for flushing queued track events
    
    * Add test for attributes and eventTags
    
    * Update tests and fix polling method in main lib
    
    * Add logging; improve track logic
    
    * Add settings for nonInteraction and to conditionally set listener; update tests
    
    * Fix nonInteraction logic; fix listener initialization; update tests
    
    * Default nonInteraction setting to true; update tests
    
    * Clean up code logic
    
    * Pin Optimizely SDK version to latest, 1.4.0
    
    * Fixes to main library; updates to tests (#2)
    
    * Fixes to main lib, update tests to check track queue flush method

--- src/main/java/com/segment/analytics/android/integrations/optimizelyx/OptimizelyXIntegration.java
+++ src/main/java/com/segment/analytics/android/integrations/optimizelyx/OptimizelyXIntegration.java
@@ -1,63 +1,223 @@
 package com.segment.analytics.android.integrations.optimizelyx;
 
-import android.app.Application;
-import android.content.Context;
-import android.os.Bundle;
+import android.os.Handler;
+
+import com.optimizely.ab.android.sdk.OptimizelyClient;
+import com.optimizely.ab.android.sdk.OptimizelyManager;
+import com.optimizely.ab.config.Experiment;
+import com.optimizely.ab.config.Variation;
+import com.optimizely.ab.notification.NotificationListener;
 import com.segment.analytics.Analytics;
+import com.segment.analytics.Options;
 import com.segment.analytics.Properties;
+import com.segment.analytics.Traits;
 import com.segment.analytics.ValueMap;
 import com.segment.analytics.integrations.IdentifyPayload;
 import com.segment.analytics.integrations.Integration;
 import com.segment.analytics.integrations.Logger;
 import com.segment.analytics.integrations.TrackPayload;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static com.segment.analytics.internal.Utils.isNullOrEmpty;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
- * Kahuna helps mobile marketers send push notifications and in-app messages.
+ * Optimizely X helps marketers and growth hackers make smart decisions through A/B testing.
  *
- * @see <a href="https://www.optimizely.com/">Pptimizely</a>
- * @see <a href="https://segment.com/docs/integrations/optimizely/">Optimizely Integration</a>
- * @see <a href="http://developers.optimizely.com/android/">Optimizely Android SDK</a>
+ * @see <a href="https://www.optimizely.com/">Optimizely</a>
+ * @see <a href="https://segment.com/docs/destinations/optimizelyx/">Optimizely Integration</a>
+ * @see <a
+ *     href="https://developers.optimizely.com/x/solutions/sdks/introduction/index.html?language=android&platform=mobile">Optimizely
+ *     Android SDK</a>
  */
 public class OptimizelyXIntegration extends Integration<Void> {
-  private static final String OPTIMIZELY_KEY = "Optimizely";
 
-  private final Context context;
+  private static final String OPTIMIZELYX_KEY = "Optimizely X";
+  NotificationListener listener;
+  private OptimizelyClient client;
   private final Logger logger;
-  private final Analytics analytics;
+  boolean trackKnownUsers;
+  static boolean nonInteraction;
+  boolean listen;
+  static final Options options = new Options().setIntegration(OPTIMIZELYX_KEY, false);
+  private Map<String, String> attributes = new HashMap<>();
+  List<TrackPayload> trackEvents = new ArrayList<>();
+  private final Handler handler = new Handler();
 
-  // Optimizely must be initialized immediately on launch. So we require the token when creating the
-  // factory, and initialize it in this method.
-//  public static Factory createFactory(final Application application, String token) {
-////    Optimizely.startOptimizelyWithAPIToken(token, application);
-////    return new Factory() {
-////      @Override public Integration<?> create(ValueMap settings, Analytics analytics) {
-////        boolean listen = settings.getBoolean("listen", false);
-////        Logger logger = analytics.logger(OPTIMIZELY_KEY);
-////
-////        OptimizelyIntegration integration =
-////            new OptimizelyIntegration(analytics, application, logger);
-////        if (listen) {
-////          Optimizely.addOptimizelyEventListener(integration);
-////        }
-////        return integration;
-////      }
-//
-////      @Override public String key() {
-////      return OPTIMIZELY_KEY;
-//////      }
-//    };
-//  }
+  public static Factory factory(OptimizelyManager manager) {
+    return new Factory(manager);
+  }
 
-  public OptimizelyXIntegration(Analytics analytics, Context context, Logger logger) {
-    this.analytics = analytics;
-    this.context = context;
+  private static class Factory implements Integration.Factory {
+    private final OptimizelyManager manager;
+
+    Factory(OptimizelyManager manager) {
+      this.manager = manager;
+    }
+
+    @Override
+    public Integration<?> create(ValueMap settings, Analytics analytics) {
+      Logger logger = analytics.logger(OPTIMIZELYX_KEY);
+      return new OptimizelyXIntegration(analytics, manager, settings, logger);
+    }
+
+    @Override
+    public String key() {
+      return OPTIMIZELYX_KEY;
+    }
+  }
+
+  public OptimizelyXIntegration(
+      final Analytics analytics, OptimizelyManager manager, ValueMap settings, Logger logger) {
+    this.client = manager.getOptimizely();
     this.logger = logger;
+
+    trackKnownUsers = settings.getBoolean("trackKnownUsers", false);
+    nonInteraction = settings.getBoolean("nonInteraction", true);
+    listen = settings.getBoolean("listen", true);
+
+    if (client.isValid()) {
+      if (listen) {
+        this.listener = createListener(analytics);
+        client.addNotificationListener(listener);
+      }
+    } else {
+      pollOptimizelyClient(manager, analytics);
+    }
   }
 
-  @Override public void identify(IdentifyPayload identify) {
+  @Override
+  public void identify(IdentifyPayload identify) {
+    super.identify(identify);
+
+    Traits traits = identify.traits();
+
+    if (!isNullOrEmpty(traits)) {
+      attributes = traits.toStringMap();
+    }
   }
 
-  @Override public void track(TrackPayload track) {
+  @Override
+  public void track(TrackPayload track) {
+    super.track(track);
+
+    int queueSize = trackEvents.size();
+
+    synchronized (this) {
+      if (!client.isValid()) {
+        logger.verbose("Optimizely not initialized. Enqueueing action: %s", track);
+        if (queueSize >= 100) {
+          logger.verbose(
+              "Event queue has exceeded limit. Dropping event at index zero: %s",
+              trackEvents.get(0));
+          trackEvents.remove(0);
+        }
+        trackEvents.add(track);
+        return;
+      }
+    }
+
+    // Segment will default sending `track` calls with `anonymousId`s since Optimizely X does not alias known and unknown users
+    // https://developers.optimizely.com/x/solutions/sdks/reference/index.html?language=objectivec&platform=mobile#user-ids
+    String userId = track.userId();
+
+    if (trackKnownUsers && isNullOrEmpty(userId)) {
+      logger.verbose(
+          "Segment will only track users associated with a userId "
+              + "when the trackKnownUsers setting is enabled.");
+      return;
+    }
+
+    String id = track.anonymousId();
+    String event = track.event();
+    Map<String, String> properties = track.properties().toStringMap();
+
+    if (trackKnownUsers && !isNullOrEmpty(userId)) {
+      id = track.userId();
+    }
+
+    client.track(event, id, attributes, properties);
+    logger.verbose("client.track(%s, %s, %s, %s)", event, id, attributes, properties);
+  }
+
+  @Override
+  public void reset() {
+    super.reset();
+
+    client.removeNotificationListener(listener);
+    logger.verbose("client.removeNotificationListener(listener)");
+  }
+
+  private void pollOptimizelyClient(final OptimizelyManager manager, final Analytics analytics) {
+    final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    final Runnable poll =
+        new Runnable() {
+          @Override
+          public void run() {
+            synchronized (OptimizelyXIntegration.this) {
+              OptimizelyXIntegration.this.client = manager.getOptimizely();
+            }
+            if (client.isValid()) {
+              handler.post(
+                  new Runnable() {
+                    @Override
+                    public void run() {
+                      setClientAndFlushTracks(analytics);
+                    }
+                  });
+              scheduler.shutdown();
+            }
+          }
+        };
+    scheduler.scheduleAtFixedRate(poll, 60, 60, SECONDS);
+  }
+
+  void setClientAndFlushTracks(Analytics analytics) {
+    this.listener = createListener(analytics);
+    client.addNotificationListener(listener);
+    logger.verbose("Flushing track queue");
+
+    for (TrackPayload t : trackEvents) {
+      track(t);
+    }
+
+    trackEvents = null;
+  }
+
+  private OptimizelyNotificationListener createListener(Analytics analytics) {
+    return new OptimizelyNotificationListener(analytics);
+  }
+
+  static class OptimizelyNotificationListener extends NotificationListener {
+    private final Analytics analytics;
+
+    public OptimizelyNotificationListener(Analytics analytics) {
+      this.analytics = analytics;
+    }
+
+    @Override
+    public void onExperimentActivated(
+        Experiment experiment, String userId, Map<String, String> attributes, Variation variation) {
+
+      Properties properties =
+          new Properties()
+              .putValue("experimentId", experiment.getId())
+              .putValue("experimentName", experiment.getKey())
+              .putValue("variationId", variation.getId())
+              .putValue("variationName", variation.getKey())
+              .putValue("nonInteraction", 1);
+
+      if (!nonInteraction) {
+        properties.remove("nonInteraction");
+      }
+      analytics.track("Experiment Viewed", properties, options);
+    }
   }
 }
--- src/test/java/com/segment/analytics/android/integrations/optimizelyx/OptimizelyXTest.java
+++ src/test/java/com/segment/analytics/android/integrations/optimizelyx/OptimizelyXTest.java
@@ -1,12 +1,20 @@
 package com.segment.analytics.android.integrations.optimizelyx;
 
-import android.app.Application;
+import com.optimizely.ab.android.sdk.OptimizelyClient;
+import com.optimizely.ab.android.sdk.OptimizelyManager;
+import com.optimizely.ab.config.Experiment;
+import com.optimizely.ab.config.LiveVariableUsageInstance;
+import com.optimizely.ab.config.TrafficAllocation;
+import com.optimizely.ab.config.Variation;
 import com.segment.analytics.Analytics;
 import com.segment.analytics.Properties;
+import com.segment.analytics.Traits;
+import com.segment.analytics.ValueMap;
 import com.segment.analytics.core.tests.BuildConfig;
 import com.segment.analytics.integrations.Logger;
 import com.segment.analytics.test.IdentifyPayloadBuilder;
 import com.segment.analytics.test.TrackPayloadBuilder;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,37 +24,178 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.rule.PowerMockRule;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import static com.segment.analytics.Analytics.LogLevel.VERBOSE;
-import static com.segment.analytics.Utils.createTraits;
-import static org.mockito.Mockito.verify;
-import static org.mockito.MockitoAnnotations.initMocks;
-import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-@RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 18, manifest = Config.NONE)
-@PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*" })
-//@PrepareForTest({ Optimizely.class }) //
+import static com.segment.analytics.Analytics.LogLevel.VERBOSE;
+import static com.segment.analytics.android.integrations.optimizelyx.OptimizelyXIntegration.OptimizelyNotificationListener;
+import static com.segment.analytics.android.integrations.optimizelyx.OptimizelyXIntegration.options;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+@PowerMockIgnore({ "org.mockito.*", "org.roboelectric.*", "android.*" })
+@PrepareForTest({ OptimizelyClient.class})
 public class OptimizelyXTest {
 
   @Rule public PowerMockRule rule = new PowerMockRule();
-  @Mock Application application;
   @Mock Analytics analytics;
+  private OptimizelyXIntegration integration;
+  private OptimizelyClient client;
+  private Map<String, String> attributes = new HashMap<>();
 
   @Before public void setUp() {
-  }
-
-  @Test public void identify() {
+    initMocks(this);
+    PowerMockito.mock(OptimizelyClient.class);
+    PowerMockito.mock(OptimizelyManager.class);
+    OptimizelyManager manager = mock(OptimizelyManager.class);
+    client = mock(OptimizelyClient.class);
+    when(manager.getOptimizely()).thenReturn(client);
+    when(client.isValid()).thenReturn(true);
+    integration = new OptimizelyXIntegration(analytics, manager, new ValueMap()
+            .putValue("trackKnownUsers", false)
+            .putValue("nonInteraction", true)
+            .putValue("listen", false),
+            Logger.with(VERBOSE));
   }
 
   @Test public void track() {
+    Properties properties = new Properties();
+    Traits traits = new Traits()
+            .putValue("userId", "123")
+            .putValue("anonymousId", "456");
+    integration.track(new TrackPayloadBuilder().properties(properties).traits(traits).event("event").build());
+
+    verify(client).track("event", "456", attributes, properties.toStringMap());
   }
 
-  @Test public void trackWithRevenue() {
+  @Test public void trackKnownUsers() {
+    integration.trackKnownUsers = true;
+
+    Properties properties = new Properties();
+    Traits traits = new Traits()
+            .putValue("userId", "123");
+    integration.track(new TrackPayloadBuilder().properties(properties).traits(traits).event("event").build());
+
+    verify(client).track("event", "123", attributes, properties.toStringMap());
   }
 
-  @Test public void root() {
+  @Test public void trackKnownUsersNoUserId() {
+    integration.trackKnownUsers = true;
+
+    Traits traits = new Traits()
+            .putValue("anonymousId", "456");
+    integration.track(new TrackPayloadBuilder().traits(traits).event("event").build());
+
+    verify(client,times(0)).track("event", "123", attributes);
+  }
+
+  @Test public void mapAttributesAndEventTags() {
+    integration.trackKnownUsers = true;
+
+    Traits traits = new Traits()
+            .putValue("userId", "123")
+            .putValue("gender", "male");
+    integration.identify(new IdentifyPayloadBuilder().traits(traits).build());
+
+    Properties properties = new Properties()
+            .putValue("foo", "bar");
+    Traits trackTraits = new Traits()
+            .putValue("userId", "123");
+    integration.track(new TrackPayloadBuilder().properties(properties).traits(trackTraits).event("event").build());
+
+    attributes.putAll(traits.toStringMap());
+
+    verify(client).track("event", "123", attributes, properties.toStringMap());
+  }
+
+  @Test public void pollOptimizelyClient() {
+    Properties properties = new Properties();
+    Traits traits = new Traits()
+            .putValue("anonymousId", "456");
+    integration.trackEvents.add(new TrackPayloadBuilder().properties(properties).traits(traits).event("event").build());
+    integration.setClientAndFlushTracks(analytics);
+
+    verify(client).track("event", "456", attributes, properties.toStringMap());
+  }
+
+  @Test public void onExperimentActivated() {
+    OptimizelyXIntegration.nonInteraction = false;
+    String id = "123";
+    String experimentKey = "experiment_key";
+    List<String> audienceIds = new ArrayList<>();
+    List<Variation> variations = new ArrayList<>();
+    Map<String, String> userIdToVariationKeyMap = new HashMap<>();
+    List<TrafficAllocation> trafficAllocation = new ArrayList<>();
+    String groupId = "123";
+
+    String variationKey = "variation_key";
+    List<LiveVariableUsageInstance> liveVariableUsageInstancess = new ArrayList<>();
+
+    Experiment experiment = new Experiment(id, experimentKey, null, null, audienceIds, variations, userIdToVariationKeyMap, trafficAllocation, groupId);
+    String userId = "123";
+    Map<String, String> attributes = new HashMap<>();
+    attributes.put("name", "brennan");
+    Variation variation = new Variation(id, variationKey, liveVariableUsageInstancess);
+
+    OptimizelyNotificationListener listener = new OptimizelyNotificationListener(analytics);
+    listener.onExperimentActivated(experiment, userId, attributes, variation);
+
+    Properties properties = new Properties()
+            .putValue("experimentId", "123")
+            .putValue("experimentName", "experiment_key")
+            .putValue("variationId", "123")
+            .putValue("variationName", "variation_key");
+
+    verify(analytics).track("Experiment Viewed", properties, options);
+  }
+
+  @Test public void nonInteractionEnabled() {
+    String id = "123";
+    String experimentKey = "experiment_key";
+    List<String> audienceIds = new ArrayList<>();
+    List<Variation> variations = new ArrayList<>();
+    Map<String, String> userIdToVariationKeyMap = new HashMap<>();
+    List<TrafficAllocation> trafficAllocation = new ArrayList<>();
+    String groupId = "123";
+
+    String variationKey = "variation_key";
+    List<LiveVariableUsageInstance> liveVariableUsageInstancess = new ArrayList<>();
+
+    Experiment experiment = new Experiment(id, experimentKey, null, null, audienceIds, variations, userIdToVariationKeyMap, trafficAllocation, groupId);
+    String userId = "123";
+    Map<String, String> attributes = new HashMap<>();
+    attributes.put("name", "brennan");
+    Variation variation = new Variation(id, variationKey, liveVariableUsageInstancess);
+
+    OptimizelyNotificationListener listener = new OptimizelyNotificationListener(analytics);
+    listener.onExperimentActivated(experiment, userId, attributes, variation);
+
+    Properties properties = new Properties()
+            .putValue("experimentId", "123")
+            .putValue("experimentName", "experiment_key")
+            .putValue("variationId", "123")
+            .putValue("variationName", "variation_key")
+            .putValue("nonInteraction", 1);
+
+    verify(analytics).track("Experiment Viewed", properties, options);
+  }
+
+  @Test public void listenerDisabled() {
+    verify(client, times(0)).addNotificationListener(integration.listener);
+  }
+
+  @Test public void reset() {
+    integration.reset();
+    verify(client).removeNotificationListener(integration.listener);
   }
 }
